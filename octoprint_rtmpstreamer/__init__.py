9eda24e7723599514673e0b909f6caa841fcc41e
Justin F. Hallett <thesin@southofheaven.org>
2022-01-18 09:29:47 -0700

    Fix for windows nul audio which wasn't valid for YouTube (#36)
    * Should alow for filters and overlays to play nicely now
    
    * Streaming fix for windows ffmpeg to youtube, maybe audio should be an option?

--- octoprint_rtmpstreamer/__init__.py
+++ octoprint_rtmpstreamer/__init__.py
@@ -63,14 +63,10 @@ class rtmpstreamer(octoprint.plugin.BlueprintPlugin,
 
         self.frame_rate_default = 5
         self.stream_resolution_default = "640x480"
-        if self.platform == "win32":
-            audio_dev = "nul"
-        else:
-            audio_dev = "/dev/zero"
 
         self.ffmpeg_cmd_default = (
             "{ffmpeg} -re -f mjpeg -framerate {frame_rate} -i {webcam_url} {filter} "  # Video input
-            "-ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i " + audio_dev + " " # Audio input
+            "-f lavfi -i anullsrc " # Audio input
             "-acodec aac -ab 128k "  # Audio output
             "-s {stream_resolution} -vcodec {videocodec} -threads {threads} -pix_fmt yuv420p -framerate {frame_rate} -g {gop_size} -vb {bitrate} -strict experimental "  # Video output
             "-f flv {stream_url}")  # Output stream
